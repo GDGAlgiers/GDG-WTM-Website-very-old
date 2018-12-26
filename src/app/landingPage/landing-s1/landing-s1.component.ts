e2c6f64d3c657211a10b68950e1fc8bc2466c9c0
Oussama <ahmedoussama.amor@etu.usthb.dz>
2018-12-26 23:29:57 +0100

    setup and configure hm carousel

--- src/app/landingPage/landing-s1/landing-s1.component.ts
+++ src/app/landingPage/landing-s1/landing-s1.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-s1.component.css']
 })
 export class LandingS1Component implements OnInit {
-
+  index = 0;
+  infinite = true;
+  direction = 'right';
+  directionToggle = true;
+  autoplay = true;
+  avatars = '1234567890'.split('').map((x, i) => {
+    const num = i;
+    // const num = Math.floor(Math.random() * 1000);
+    return {
+      url: `https://picsum.photos/600/400/?${num}`,
+      title: `${num}`
+    };
+  });
   constructor() { }
 
   ngOnInit() {
   }
 
+  indexChanged(index) {
+    console.log(index);
+  }
 }
