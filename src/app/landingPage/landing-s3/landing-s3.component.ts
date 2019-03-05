6a44851c30ec1cc1aa09012f114908a5217fee4d
hocinekhen <hocinekhen@yahoo.com>
2019-03-05 19:59:22 +0100

    modified the design of landing s3 and make it as described in the design

--- src/app/landingPage/landing-s3/landing-s3.component.ts
+++ src/app/landingPage/landing-s3/landing-s3.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-landing-s3',
   templateUrl: './landing-s3.component.html',
@@ -8,8 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class LandingS3Component implements OnInit {
 
   constructor() { }
-
   ngOnInit() {
+    $(".titleDiv").on('click', function(event){
+      let parent=event.target.parentElement;
+      var titleElements = document.getElementsByClassName("show");
+      console.log(titleElements[0])
+      for (var i = 0, len = titleElements.length; i < len; i++) {
+          if(parent!=titleElements[i])
+          {
+              titleElements[i].classList.add("hide")
+          titleElements[i].classList.remove("show")
+          }
+          
+      }
+      
+      parent.classList.toggle("show");
+      parent.classList.toggle("hide")
+      //check if there is at least one div opened else open the last one
+    titleElements = document.getElementsByClassName("show");
+    if(titleElements.length==0)
+    {
+        var divContainers=document.getElementsByClassName("divcontainer");
+        divContainers[divContainers.length-1].classList.add("show");
+        divContainers[divContainers.length-1].classList.remove("hide");
+    }
+  });
   }
 
 }
