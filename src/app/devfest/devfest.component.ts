1404203116adae5524972432cd770b41fbd97488
yacinebenkaidali <fy_benkaidali@esi.dz>
2019-08-14 18:20:01 +0100

    Fixed location bugs and currently updating the content

--- src/app/devfest/devfest.component.ts
+++ src/app/devfest/devfest.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit,AfterViewInit } from '@angular/core';
-import  SimpleBar from 'simplebar'
+import { Component, OnInit, AfterViewInit, NgModule } from '@angular/core';
+import SimpleBar from 'simplebar'
+import { BrowserModule } from '@angular/platform-browser';
+import { AgmCoreModule } from '@agm/core';
 @Component({
   selector: 'app-devfest',
   templateUrl: './devfest.component.html',
   styleUrls: ['./devfest.component.css']
 })
-export class DevfestComponent implements OnInit,AfterViewInit {
+export class DevfestComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     new SimpleBar(document.querySelector('.simpleBar'), { autoHide: true });
   }
-  lat: number = 51.678418;
-  lng: number = 7.809007;
-  constructor() { }
-
+  lat: number = 36.705074;
+  lng: number = 3.173912;
+  zoom: number = 16;
   ngOnInit() {
   }
 
 }
+@NgModule({
+  imports: [
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCSL3loqg7Q7tRLNiZ4xd230pF7KSIFQv0'
+    })
+  ],
+  declarations: [DevfestComponent],
+  bootstrap: [DevfestComponent]
+})
+export class AppModule { }
